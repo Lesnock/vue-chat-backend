7101ae78fd56ea48a5fe02ba38ec22fd47d0f9ce
METADIL MÓVEIS - Caio Lesnock <caio@metadil.com.br>
2020-08-11 11:11:37 -0300

    Fix search messages

--- src/controllers/MessagesController.js
+++ src/controllers/MessagesController.js
@@ -9,11 +9,14 @@ class MessagesController extends Controller {
 
     try {
       const messages = await db('messages')
-        .where('sender_id', loggedUserId)
-        .where('recipient_id', receiverId)
+        .whereRaw(
+          '(sender_id = ? AND recipient_id = ?) OR (sender_id = ? AND recipient_id = ?)',
+          [loggedUserId, receiverId, receiverId, loggedUserId]
+        )
 
       return res.json(messages)
     } catch (error) {
+      console.log(error)
       return res.status(500).json({ error: error.message })
     }
 
